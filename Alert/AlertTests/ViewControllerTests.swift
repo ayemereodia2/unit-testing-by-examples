a3f0de4ba1148958e73c689f97e6cbf8db25c039
Ayemere <davidodia4@gmail.com>
2022-11-24 18:49:25 +0100

    added test for alert actions

--- Alert/AlertTests/ViewControllerTests.swift
+++ Alert/AlertTests/ViewControllerTests.swift
@@ -12,23 +12,30 @@ import ViewControllerPresentationSpy
 class ViewControllerTests: XCTestCase {
 
     private var alertVerifer: AlertVerifier!
+    private var sut : ViewController?
     
     override func setUp() {
         super.setUp()
         alertVerifer = AlertVerifier()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
     }
     
     override func tearDown() {
         alertVerifer = nil
+        sut = nil
         super.tearDown()
     }
 
     func test_tappingButton_shouldShowAlert() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-        sut.loadViewIfNeeded()
         
-        tap(sut.button)
+        sut?.loadViewIfNeeded()
+        guard let button = sut?.button else {
+            XCTFail("Button does not exist")
+            return
+        }
+        
+        tap(button)
         
         alertVerifer.verify(
             title: "Do the thing?",
@@ -39,5 +46,17 @@ class ViewControllerTests: XCTestCase {
             presentingViewController: sut)
         
         XCTAssertEqual(alertVerifer.preferredAction?.title, "Ok", "preferred action")
+    }
+    
+    func test_executeAlertAction_withOKButton() throws {
+        sut?.loadViewIfNeeded()
+        guard let button = sut?.button else {
+            XCTFail("Button does not exist")
+            return
+        }
+        
+        tap(button)
+        
+        try alertVerifer.executeAction(forButton: "Ok")
     }
 }
